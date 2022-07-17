562c77a6130c8692df8cd2e33501c9554941967a
JhonWester <jhonjameshernandez63@gmail.com>
2022-07-17 14:19:27 -0500

    Add class to screen project

---
+++ Models/Screen.py
@@ -0,0 +1,19 @@
+from machine import Pin, I2C
+from Package.ssd1306 import SSD1306_I2C
+
+class Screen:
+    
+    height = 128
+    width = 64
+    
+    def __init__(self, scl, sda) -> None:
+        self.__i2c = I2C(0, scl = Pin(scl), sda = Pin(sda))
+        self.__oled = SSD1306_I2C(self.__class__.width, self.__class__.height, self.__i2c)
+        
+    def FillMessage(self, colum, file, message):
+        self.__oled.text(message, colum, file)
+        self.__oled.show()
+    
+    def ShowMessage(self):
+        self.__oled.show()
+        
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # main.py
 import time
 
-# Package
+# Models
 from Models.SensorDht import SensorDHT
 from Models.FireConnect import FirebaseConnect
 from Models.PowerBomb import PowerBomb
 from Models.SensorAnalogo import SensorAnalogo
 from Models.Led import Led
+from Models.Screen import Screen
+
+#Package
 from Shared.NetworkConnect import NetworkConnection
 
 red = {'name': 'TP-Link_D54E', 'pass': '04577041'}
@@ -29,6 +32,9 @@ sensorFT = SensorAnalogo(36, 620, 1023)
 #Sensor de luz (pin, minimo, maximo)
 sensorLDR = SensorAnalogo(39, 65, 650)
 
+#Pantalla oled (Pin scl, Pin sda)
+screen = Screen(22, 21)
+
 #Salidas led
 ledBomb = Led(2)
 ledHumidity = Led(4)
@@ -41,14 +47,21 @@ valueFC = 0
 valueLDR = 0
 
 def main():
+    
+    #Show init message
+    screen.FillMessage("Bienvenido a BE_AGRO!!!", 0 , 10)
+    screen.ShowMessage()
+    
     try:
         if connection.conectaWifi():
             
-            
             while True:
                 proccessBomb(False)
+                
                 environment = proccessDHT()
-                groundMoisture= proccessFC()
+                
+                groundMoisture = proccessFC()
+                
                 light = proccessLDR()
                 
                 if (light > 80 and groundMoisture < 50):
@@ -59,22 +72,27 @@ def main():
                         proccessBomb(True)
                     else:
                         proccessBomb(False)
-                        
+
+                screen.ShowMessage()
                 time.sleep(5)
                     
         else:
             print ("Imposible conectar")
-            #oled.text("Imposible conectar!!!", 0 , 1) # columna ---- fila
-            #oled.show()
+            screen.FillMessage(0 , 20, "Imposible conectar!!!")
+            screen.ShowMessage()
             connection.desactive()
     except:
         print ("Ocurrio un problema!!!")
+        screen.FillMessage(0 , 20, "Ocurrio un problema!!!")
+        screen.ShowMessage()
+
 
 #Ejecucion proceso de sensor dht11
 def proccessDHT():
     time.sleep(4)
     sensorDHT.getData()
     messageDHT = "T={:02.} ºC, H={:02d}%".format(sensorDHT.temperature, sensorDHT.humidity)
+    screen.FillMessage(0, 20, messageDHT)
     
     if (valueDHT[0] != sensorDHT.humidity | valueDHT[1] != sensorDHT.temperature):
         valueDHT[0] = sensorDHT.humidity
@@ -88,10 +106,12 @@ def proccessBomb(state):
     time.sleep(4)
     if state == True:
         powerBomb.BombOn()
-        print("Bomba de agua activa")
+        screen.FillMessage(0, 60, "Bomba de agua activa")
+        #print("Bomba de agua activa")
     else:
         powerBomb.BombOff()
-        print("Bomba Apagada")
+        screen.FillMessage(0, 60, "Bomba Apagada")
+        #print("Bomba Apagada")
 
 #Ejecucion proceso de sensor de humedad de tierra
 def proccessFC():
@@ -104,6 +124,7 @@ def proccessFC():
         ledHumidity.ledOff()
         
     messageFC = "{}% Humedad de suelo".format(stateSensor)
+    screen.FillMessage(0, 20, messageFC)
     
     if (valueFC != stateSensor):
         valueFC = stateSensor
@@ -116,6 +137,7 @@ def proccessLDR():
     time.sleep(4)
     stateSensor = sensorLDR.map() 
     messageLDR = "{}% nivel de luz".format(stateSensor)
+    screen.FillMessage(0, 50, messageLDR)
     
     if (valueLDR != stateSensor):
         valueLDR = stateSensor
